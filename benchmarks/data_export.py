565c08dfce5c9dac29b015391c751b61f23d542c
Matej-Chmel <58189701+Matej-Chmel@users.noreply.github.com>
2022-04-07 10:52:48 +0200

    Untested script for building LaTeX group plots.

--- benchmarks/data_export.py
+++ benchmarks/data_export.py
@@ -1,38 +1,50 @@
-import argparse
+from argparse import ArgumentParser
 import csv
-
+from dataclasses import dataclass
+from pathlib import Path
 from ann_benchmarks.datasets import DATASETS, get_dataset
 from ann_benchmarks.plotting.utils  import compute_metrics_all_runs
 from ann_benchmarks.results import load_all_results
 
+@dataclass
+class Args:
+	outputPath: Path
+	recompute: bool
+
+def exportData(a: Args):
+	datasets = DATASETS.keys()
+	dfs = []
+
+	for dataset_name in datasets:
+		print("Looking at dataset", dataset_name)
+
+		if len(list(load_all_results(dataset_name))) > 0:
+			results = load_all_results(dataset_name)
+			dataset, _ = get_dataset(dataset_name)
+			results = compute_metrics_all_runs(dataset, results, a.recompute)
+
+			for res in results:
+				res["dataset"] = dataset_name
+				dfs.append(res)
+
+	if len(dfs) > 0:
+		with a.outputPath.open("w", encoding="utf-8", newline="") as csvfile:
+			names = list(dfs[0].keys())
+			writer = csv.DictWriter(csvfile, fieldnames=names)
+			writer.writeheader()
+
+			for res in dfs:
+				writer.writerow(res)
+
+def getArgs():
+	p = ArgumentParser()
+	p.add_argument("--output", help="Path to the output file", required=True)
+	p.add_argument("--recompute", action="store_true", help="Recompute metrics")
+	args = p.parse_args()
+	return Args(Path(args.output), args.recompute)
+
+def main():
+	exportData(getArgs())
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--output',
-        help='Path to the output file',
-        required=True)
-    parser.add_argument(
-        '--recompute',
-        action='store_true',
-        help='Recompute metrics')
-    args = parser.parse_args()
-
-    datasets = DATASETS.keys()
-    dfs = []
-    for dataset_name in datasets:
-        print("Looking at dataset", dataset_name)
-        if len(list(load_all_results(dataset_name))) > 0:
-            results = load_all_results(dataset_name)
-            dataset, _ = get_dataset(dataset_name)
-            results = compute_metrics_all_runs(dataset, results, args.recompute)
-            for res in results:
-                res['dataset'] = dataset_name
-                dfs.append(res)
-    if len(dfs) > 0:
-        with open(args.output, 'w', newline='') as csvfile:
-            names = list(dfs[0].keys())
-            writer = csv.DictWriter(csvfile, fieldnames=names)
-            writer.writeheader()
-            for res in dfs:
-                writer.writerow(res)
-
+	main()
