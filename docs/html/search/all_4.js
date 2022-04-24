73ebaac265523e4fcd78553d10a0ba5e59d3853a
Matej-Chmel <58189701+Matej-Chmel@users.noreply.github.com>
2022-04-24 14:06:05 +0200

    Generated docs after the removal of parameters.

--- docs/html/search/all_4.js
+++ docs/html/search/all_4.js
@@ -21,10 +21,10 @@ var searchData=
   ['euclideandistance_2ehpp_18',['euclideanDistance.hpp',['../euclidean_distance_8hpp.html',1,'']]],
   ['euclideandistance16avx_19',['euclideanDistance16AVX',['../namespacechm.html#aa64ca5c0512021227b6e8edf530639fc',1,'chm']]],
   ['euclideandistance16avx512_20',['euclideanDistance16AVX512',['../namespacechm.html#a6cbd4d56f0da12f722f55e490e3544de',1,'chm']]],
-  ['euclideandistance16residualavx_21',['euclideanDistance16ResidualAVX',['../namespacechm.html#a7a36bbc739c565696703e8e1fbd29fc1',1,'chm']]],
-  ['euclideandistance16residualavx512_22',['euclideanDistance16ResidualAVX512',['../namespacechm.html#ab490b3f9404bb2b0ccbcb10d6c793963',1,'chm']]],
-  ['euclideandistance16residualsse_23',['euclideanDistance16ResidualSSE',['../namespacechm.html#a5bf9d1f954130cd5217fbd969e4dc09c',1,'chm']]],
+  ['euclideandistance16residualavx_21',['euclideanDistance16ResidualAVX',['../namespacechm.html#a6be30fdf02c9cc0c09ca726cb8d9b4f8',1,'chm']]],
+  ['euclideandistance16residualavx512_22',['euclideanDistance16ResidualAVX512',['../namespacechm.html#ab2ccedf9ca5ba984554a77f226f7dce4',1,'chm']]],
+  ['euclideandistance16residualsse_23',['euclideanDistance16ResidualSSE',['../namespacechm.html#ae8aaa888331b74f27b6d0a1fa3b7756f',1,'chm']]],
   ['euclideandistance16sse_24',['euclideanDistance16SSE',['../namespacechm.html#aea62b6ed85ca4f260ca8b86cbc7756f8',1,'chm']]],
-  ['euclideandistance4residualsse_25',['euclideanDistance4ResidualSSE',['../namespacechm.html#aba8d0f92ac377aebf8d3e431930ae8f1',1,'chm']]],
+  ['euclideandistance4residualsse_25',['euclideanDistance4ResidualSSE',['../namespacechm.html#a81c1d3efe3ab304097bac16cf3f06e36',1,'chm']]],
   ['euclideandistance4sse_26',['euclideanDistance4SSE',['../namespacechm.html#a1bdeb40d27f44390f284558b45d1bbf0',1,'chm']]]
 ];
--- docs/html/search/functions_3.js
+++ docs/html/search/functions_3.js
@@ -4,10 +4,10 @@ var searchData=
   ['euclideandistance_1',['euclideanDistance',['../namespacechm.html#a916adb6bae694df421c08bf5497872c7',1,'chm']]],
   ['euclideandistance16avx_2',['euclideanDistance16AVX',['../namespacechm.html#aa64ca5c0512021227b6e8edf530639fc',1,'chm']]],
   ['euclideandistance16avx512_3',['euclideanDistance16AVX512',['../namespacechm.html#a6cbd4d56f0da12f722f55e490e3544de',1,'chm']]],
-  ['euclideandistance16residualavx_4',['euclideanDistance16ResidualAVX',['../namespacechm.html#a7a36bbc739c565696703e8e1fbd29fc1',1,'chm']]],
-  ['euclideandistance16residualavx512_5',['euclideanDistance16ResidualAVX512',['../namespacechm.html#ab490b3f9404bb2b0ccbcb10d6c793963',1,'chm']]],
-  ['euclideandistance16residualsse_6',['euclideanDistance16ResidualSSE',['../namespacechm.html#a5bf9d1f954130cd5217fbd969e4dc09c',1,'chm']]],
+  ['euclideandistance16residualavx_4',['euclideanDistance16ResidualAVX',['../namespacechm.html#a6be30fdf02c9cc0c09ca726cb8d9b4f8',1,'chm']]],
+  ['euclideandistance16residualavx512_5',['euclideanDistance16ResidualAVX512',['../namespacechm.html#ab2ccedf9ca5ba984554a77f226f7dce4',1,'chm']]],
+  ['euclideandistance16residualsse_6',['euclideanDistance16ResidualSSE',['../namespacechm.html#ae8aaa888331b74f27b6d0a1fa3b7756f',1,'chm']]],
   ['euclideandistance16sse_7',['euclideanDistance16SSE',['../namespacechm.html#aea62b6ed85ca4f260ca8b86cbc7756f8',1,'chm']]],
-  ['euclideandistance4residualsse_8',['euclideanDistance4ResidualSSE',['../namespacechm.html#aba8d0f92ac377aebf8d3e431930ae8f1',1,'chm']]],
+  ['euclideandistance4residualsse_8',['euclideanDistance4ResidualSSE',['../namespacechm.html#a81c1d3efe3ab304097bac16cf3f06e36',1,'chm']]],
   ['euclideandistance4sse_9',['euclideanDistance4SSE',['../namespacechm.html#a1bdeb40d27f44390f284558b45d1bbf0',1,'chm']]]
 ];
